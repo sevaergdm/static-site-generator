e8090b8dca9a8ec8616af5157ff5e566ed68e0ab
Michael Greaves <mdgmash@gmail.com>
2025-08-12 15:48:13 +0200

    restructure and publish for web

--- src/generate_page.py
+++ src/generate_page.py
@@ -4,7 +4,7 @@ from block_markdown import extract_title
 from htmlnode import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     print(f"Generating page from {from_path} to {
           dest_path} using {template_path}")
 
@@ -21,8 +21,10 @@ def generate_page(from_path, template_path, dest_path):
     converted_markdown = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
 
-    template = template.replace("{{ Title }}", title).replace(
-        "{{ Content }}", converted_markdown)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", converted_markdown)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="', f'src="{basepath}')
 
     dest_dir_path = os.path.dirname(abs_dest_path)
     if dest_dir_path != "":
@@ -32,7 +34,10 @@ def generate_page(from_path, template_path, dest_path):
         f.write(template)
 
 
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursive(dir_path_content,
+                            template_path,
+                            dest_dir_path,
+                            basepath="/"):
     abs_dir_path_content = os.path.abspath(dir_path_content)
     abs_template_path = os.path.abspath(template_path)
     abs_dest_dir_path = os.path.abspath(dest_dir_path)
@@ -49,4 +54,5 @@ def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
             new_target = os.path.join(abs_dest_dir_path, item)
             if not os.path.exists(new_target):
                 os.mkdir(new_target)
-            generate_page_recursive(path, abs_template_path, new_target)
+            generate_page_recursive(
+                path, abs_template_path, new_target, basepath)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,21 @@
 import os
 from shutil import rmtree
+import sys
 
 from copy_content import copy_content
 from generate_page import generate_page_recursive
 
 
 def main():
+    args = sys.argv
+
+    if args[1]:
+        basepath = args[1]
+    else:
+        basepath = "/"
+
     source_dir = os.path.abspath("static")
-    target_dir = os.path.abspath("public")
+    target_dir = os.path.abspath("docs")
 
     if os.path.exists(target_dir):
         rmtree(target_dir)
@@ -15,7 +23,7 @@ def main():
 
     copy_content(source_dir, target_dir)
     generate_page_recursive(
-        "content", "template.html", "public")
+        "content", "template.html", "docs", basepath)
 
 
 if __name__ == "__main__":
